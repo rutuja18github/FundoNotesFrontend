3a2ad836966c6169c3c5cc129e25f001ea06012b
RutujaPatil18 <prutuja189@gmail.com>
2022-12-14 13:00:27 +0530

    [Rutuja]Added:Define path for each component and use placeholder called router outlet to navigate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './Components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+import {ResetPasswordComponent} from './Components/reset-password/reset-password.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'forgotPassword' ,component:ForgotPasswordComponent},
+  {path:'resetPassword',component:ResetPasswordComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
