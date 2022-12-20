5cda3a041e962e70ad6c2b7dec2349b167ec8805
RutujaPatil18 <prutuja189@gmail.com>
2022-12-20 08:17:18 +0530

    [Rutuja]Added: Design icons for create note

---
+++ src/app/Components/icon/icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss']
+})
+export class IconComponent {
+
+
+}
